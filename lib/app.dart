c6e5911dcd7be952726094684328fe5c402c087b
a <a@MacBook-Pro-de-ayoub.local>
2024-10-26 15:50:47 +0100

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,31 @@
 
 import 'package:flutter/material.dart';
-import 'package:weatherapp/core/databases/cache/cache_helper.dart';
-import 'package:weatherapp/main.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:weatherapp/features/weather/presentation/cubit/weather_cubit.dart';
+import 'package:weatherapp/features/weather/presentation/screens/search_screen.dart';
 
+class App extends StatelessWidget {
+  const App({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(430, 932), // Added const for optimization
+      minTextAdapt: true,
+      builder: (context, child) {
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await CacheHelper().init();
-  runApp(App());
+        return BlocProvider(
+          create: (context) => WeatherCubit(),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              useMaterial3: true,
+            ),
+            home: const SearchScreen(),
+          ),
+        );
+      },
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,12 @@
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:weatherapp/features/weather/presentation/cubit/weather_cubit.dart';
-import 'package:weatherapp/features/weather/presentation/screens/search_screen.dart';
+import 'package:weatherapp/app.dart';
+import 'package:weatherapp/core/databases/cache/cache_helper.dart';
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(430, 932), // Added const for optimization
-      minTextAdapt: true,
-      builder: (context, child) {
 
-        return BlocProvider(
-          create: (context) => WeatherCubit(),
-          child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              useMaterial3: true,
-            ),
-            home: const SearchScreen(),
-          ),
-        );
-      },
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper().init();
+  runApp(App());
 }
